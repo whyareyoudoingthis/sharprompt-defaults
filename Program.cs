7f7e87d4edb1dc5ff7750385d4f44e4890bcabb8
Alistair Chapman <alistair@agchapman.com>
2020-11-15 01:14:36 +1000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Sharprompt.Validations;
+
+namespace sharprompt_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userName = Sharprompt.Prompt.Input<string>("Please enter a name", "default", new[] { FakeValidator() });
+            Console.WriteLine($"Entered {userName}");
+        }
+
+        internal static Func<object, ValidationResult> FakeValidator() => (obj) => {
+            return string.IsNullOrWhiteSpace(obj.ToString()) ? new ValidationResult("Received empty string") : null;
+        };
+    }
+}
